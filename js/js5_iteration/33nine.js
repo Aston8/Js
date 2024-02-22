83d57a3ff04ea922282be6d3aa646a4ac8e9dcbb
Aston Monteiro <astonmonteiro08@gmail.com>
2024-02-22 21:34:12 +0530

    Update 33nine.js

--- js/js5_iteration/33nine.js
+++ js/js5_iteration/33nine.js
@@ -1,34 +1,43 @@
+//in reduce we get accumulator and current value
+
 const myNums = [1, 2, 3]
 
-// const myTotal = myNums.reduce(function (acc, currval) {
-//     console.log(`acc: ${acc} and currval: ${currval}`);
-//     return acc + currval
-// }, 0)
+ const myTotal = myNums.reduce(function (acc, currval) {
+     console.log(`acc: ${acc} and currval: ${currval}`);
+     return acc + currval
+ }, 0)//0 initial value
+  console.log(myTotal);
 
-const myTotal = myNums.reduce( (acc, curr) => acc+curr, 0)
+//arrow function
+ const myTotal1 = myNums.reduce( (acc, curr) => acc+curr, 0)//0 intial value
 
-console.log(myTotal);
+ console.log(myTotal1);
+
+
+
+
+
 
 
 const shoppingCart = [
-    {
-        itemName: "js course",
-        price: 2999
-    },
-    {
-        itemName: "py course",
-        price: 999
-    },
-    {
-        itemName: "mobile dev course",
-        price: 5999
-    },
-    {
-        itemName: "data science course",
-        price: 12999
-    },
-]
+     {
+         itemName: "js course",
+         price: 2999
+     },
+     {
+         itemName: "py course",
+         price: 999
+     },
+     {
+         itemName: "mobile dev course",
+         price: 5999
+     },
+     {
+         itemName: "data science course",
+         price: 12999     
+     }, 
+    ]
 
 const priceToPay = shoppingCart.reduce((acc, item) => acc + item.price, 0)
 
-console.log(priceToPay);
+ console.log(priceToPay);
