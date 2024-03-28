44d3bd18482965894cba58fb1e47bf889df6b6a4
Aston Monteiro <astonmonteiro08@gmail.com>
2024-03-28 13:21:26 +0530

    Update 44promices.js

--- js/js8_advanced_one/44promices.js
+++ js/js8_advanced_one/44promices.js
@@ -112,18 +112,25 @@ consumePromiseFive()
 
 
 
-// async function getAllUsers(){
-//     try {
-//         const response = await fetch('https://jsonplaceholder.typicode.com/users')
+ async function getAllUsers(){
+     try {
+         const response = await fetch('https://jsonplaceholder.typicode.com/users')
 
-//         const data = await response.json()
-//         console.log(data);
-//     } catch (error) {
-//         console.log("E: ", error);
-//     }
-// }
+         const data = await response.json()
+         console.log(data);
+     } catch (error) {
+         console.log("E: ", error);
+     }
+ }
 
-//getAllUsers()
+getAllUsers()
+
+
+
+
+
+
+//using .then and .cache
 
 fetch('https://api.github.com/users/hiteshchoudhary')
 .then((response) => {
