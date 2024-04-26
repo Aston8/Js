7f54afcf1f0c4fadb5283eb65a54d2bb81ad2125
Aston Monteiro <astonmonteiro08@gmail.com>
2024-04-26 13:25:07 +0530

    Create 56objectgetset.js

---
+++ js/js9_classes_and_objects/56objectgetset.js
@@ -0,0 +1,19 @@
+//get set using ubjects
+
+
+const User = {
+    _email: 'h@hc.com',
+    _password: "abc",
+
+
+    get email(){
+        return this._email.toUpperCase()
+    },
+
+    set email(value){
+        this._email = value
+    }
+}
+
+const tea = Object.create(User) //creating object in basis of user
+console.log(tea.email);
